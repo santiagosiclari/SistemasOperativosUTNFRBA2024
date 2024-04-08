cdb4536d74b05f6676674992a996b69e7c0bb93a
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2024-04-08 00:07:50 -0300

    Declaracion variables generales

--- utils/src/utils/hello.h
+++ utils/src/utils/hello.h
@@ -4,10 +4,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include<commons/log.h>
+#include<commons/config.h>
+#include<commons/string.h>
+#include<commons/string.h>
+
 /**
 * @fn    decir_hola
 * @brief Imprime un saludo al nombre que se pase por parámetro por consola.
 */
 void decir_hola(char* quien);
+
+//VARIABLES GENERALES
+t_log* logger;
+t_log* debug_log;
+t_config* config;
+
+//declaracion funciones
+int crear_conexion(char* ip, char* puerto);
+
+
+
+
+
+
+
+
 
 #endif
