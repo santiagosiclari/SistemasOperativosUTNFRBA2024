de8f765abe9b3beb3de5a07dcc0f4b9b93fa0834
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2024-04-08 00:06:36 -0300

    Declaracion variables config

---
+++ memoria/src/main.h
@@ -0,0 +1,18 @@
+#ifndef MEMORIA_MAIN_H_
+#define MEMORIA_MAIN_H_
+
+#include<commons/log.h>
+#include<commons/config.h>
+
+char* PUERTO_ESCUCHA; //pdf dice int
+int TAM_MEMORIA;
+int TAM_PAGINA;
+char* PATH_INSTRUCCIONES;
+int RETARDO_RESPUESTA;
+
+t_log* memoria_logger;
+t_log* memoria_debug;
+t_config* memoria_config;
+
+
+#endif
