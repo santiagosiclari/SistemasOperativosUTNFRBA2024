5c41959b1ff7d3abdfab1302ca34250fb0b533dc
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2024-04-08 00:05:34 -0300

    Declaracion variables config

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -2,7 +2,38 @@
 #include <stdio.h>
 #include <utils/hello.h>
 
+#include<main.h>
 int main(int argc, char* argv[]) {
-    decir_hola("una Interfaz de Entrada/Salida");
-    return 0;
+    //decir_hola("una Interfaz de Entrada/Salida");
+
+entradasalida_logger = log_create(,,,LOG_LEVEL_INFO);
+if(entradasalida_logger == NULL){
+    perror("Algo raro paso con el log. No se pudo crear o encontrar el archivo.");
+    exit(EXIT_FAILURE);
+}
+
+entradasalida_debug = log_create(,,,LOG_LEVEL_TRACE);
+if(entradasalida_debug == NULL){
+    perror("Algo raro paso con el log. No se pudo crear o encontrar el archivo.");
+    exit(EXIT_FAILURE);
+}
+
+entradasalida_config = config_create("/home/utnso/tp-2024-1c-ChacoForSystem/entradasalida/entradasalida.config");
+if (entradasalida_config == NULL)
+{
+    perror("Error al intertar cargar el config.");
+    exit(EXIT_FAILURE);
+}
+
+TIPO_INTERFAZ = config_get_string_value(entradasalida_config, "TIPO_INTERFAZ");
+TIEMPO_UNIDAD_TRABAJO = config_get_int_value(entradasalida_config, "TIEMPO_UNIDAD_TRABAJO");
+IP_KERNEL = config_get_string_value(entradasalida_config, "IP_KERNEL");
+PUERTO_KERNEL = config_get_string_value(entradasalida_config, "PUERTO_KERNEL");
+IP_MEMORIA = config_get_string_value(entradasalida_config, "IP_MEMORIA");
+PUERTO_MEMORIA = config_get_string_value(entradasalida_config, "PUERTO_MEMORIA");
+PATH_BASE_DIALFS = config_get_string_value(entradasalida_config, "PATH_BASE_DIALFS");
+BLOCK_SIZE = config_get_int_value(entradasalida_config, "BLOCK_SIZE");
+BLOCK_COUNT = config_get_int_value(entradasalida_config, "BLOCK_COUNT");
+
+return 0;
 }
---
+++ entradasalida/src/main.h
@@ -0,0 +1,23 @@
+#ifndef ENTRADASALIDA_MAIN_H_
+#define ENTRADASALIDA_MAIN_H_
+
+#include<commons/log.h>
+#include<commons/config.h>
+
+
+//declaracion archivos de configuracion
+char* TIPO_INTERFAZ;
+int TIEMPO_UNIDAD_TRABAJO;
+char* IP_KERNEL;
+char* PUERTO_KERNEL; //pdf dice int?
+char* IP_MEMORIA;
+char* PUERTO_MEMORIA;//pdf dice int?
+char* PATH_BASE_DIALFS;
+int BLOCK_SIZE;
+int BLOCK_COUNT;
+
+t_log* entradasalida_logger;
+t_log* entradasalida_debug;
+t_config* entradasalida_config;
+
+#endif
