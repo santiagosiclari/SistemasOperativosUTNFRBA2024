cfc9cd88e0ca84f71ce3445dbe27f3f014bb77cd
Joaquin Mariosa <jmariosa@frba.utn.edu.ar>
2024-04-11 23:19:37 +0000

    Saco parametro de ip en iniciar_servidro(...)

--- utils/include/sockets.h
+++ utils/include/sockets.h
@@ -28,7 +28,7 @@ typedef struct
 } t_paquete;
 
 int crear_conexion(t_log* logger, char *ip, char* puerto, char* modulo);
-int iniciar_servidor(t_log* logger, char* ip, char* puerto, char* modulo);
+int iniciar_servidor(t_log* logger, char* puerto, char* modulo);
 int esperar_cliente(t_log* logger, int socket_servidor, char* modulo);
 int recibir_operacion(t_log* logger, int socket_cliente, char* ip, char* puerto, char* modulo);
 void liberar_conexion(int socket_cliente);
