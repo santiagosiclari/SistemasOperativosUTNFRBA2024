dc35a8185f1fcc681f67281976fafdcfb0f3bc4b
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2024-04-12 19:23:15 -0300

    Agrego conexion kernel - memoria

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -1,28 +1,29 @@
 #include "../include/main.h"
 
 int main(int argc, char* argv[]) {
-    
+
     // Inicializamos logger y logger debug
     init_memoria_logs();
 
     // Inicializamos config
     init_memoria_config();
 
-    // Iniciar servidor de memoria
-    fd_memoria = iniciar_servidor(memoria_logger, PUERTO_ESCUCHA, "Memoria");
+    // Conexiones
+
+    //iniciar servidor
+    fd_memoria = iniciar_servidor(memoria_logger, PUERTO_ESCUCHA, "Memoria iniciada");
 
     // Conexiones
     // Espera conexion de Kernel
-
-
-    // Espera conexion de EntradaSalida
-
+    fd_kernel = esperar_cliente(memoria_logger, fd_memoria, "Kernel");
 
     // Espera conexion de CPU
     log_info(memoria_logger, "Esperando al modulo CPU");
     fd_cpu = esperar_cliente(memoria_logger, fd_memoria, "CPU");
 
-    
+    //ESPERAR CONEXION I/O
+    //fd_entradasalida = esperar_cliente(memoria_logger, fd_memoria, "I/O");
+
 
     return 0;
 }
