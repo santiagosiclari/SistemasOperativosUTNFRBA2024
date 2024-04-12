4adb0d7f4a6c0ab89afe79c1e36bd40171ad6f36
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2024-04-12 19:31:25 -0300

    Correccion direccion config

--- kernel/src/configs.c
+++ kernel/src/configs.c
@@ -1,7 +1,7 @@
 #include <../include/configs.h>
 
 void init_kernel_config(void) {
-    kernel_config = config_create("/home/utnso/tp-2024-1c-ChacoForSystem/kernell/kernel.config");
+    kernel_config = config_create("/home/utnso/tp-2024-1c-ChacoForSystem/kernel/kernel.config");
     if (kernel_config == NULL) {
         perror("Error al intertar cargar el config.");
         exit(EXIT_FAILURE);
@@ -16,7 +16,7 @@ void init_kernel_config(void) {
     PUERTO_CPU_INTERRUPT = config_get_string_value(kernel_config, "PUERTO_CPU_INTERRUPT");
     ALGORITMO_PLANIFICACION = config_get_string_value(kernel_config, "ALGORITMO_PLANIFICACION");
     QUANTUM = config_get_int_value(kernel_config, "QUANTUM");
-    RECURSOS = config_get_array_value(kernel_config, "RECUERSOS");
-    INSTANCIAS_RECURSOS = config_get_array_value(kernel_config, "INSTANCIAS_RECUERSOS");
+    RECURSOS = config_get_array_value(kernel_config, "RECURSOS");
+    INSTANCIAS_RECURSOS = config_get_array_value(kernel_config, "INSTANCIAS_RECURSOS");
     GRADO_MULTIPROGRAMACION = config_get_int_value(kernel_config, "GRADO_MULTIPROGRAMACION");
 }
