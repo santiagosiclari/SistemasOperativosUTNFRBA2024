f0ff4cfcfb74be94e5c4e64f6f6595100938a3a2
Santiago Nicolas Siclari <ssiclari@frba.utn.edu.ar>
2024-04-12 19:35:42 -0300

    Agrego variables y file descriptor

--- kernel/include/main.h
+++ kernel/include/main.h
@@ -1,11 +1,35 @@
-#ifndef KERNELL_MAIN_H_
-#define KERNELL_MAIN_H_
+#ifndef KERNEL_MAIN_H_
+#define KERNEL_MAIN_H_
 
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "configs.h"
 #include "logs.h"
+
 #include <../../utils/include/sockets.h>
+
+int fd_kernel;
+int fd_memoria;
+int fd_entradasalida;
+int fd_cpu_dispatch;
+int fd_cpu_interrupt;
+
+t_config* kernel_config;
+
+char* PUERTO_ESCUCHA; //pdf dice int
+char* IP_MEMORIA;
+char* PUERTO_MEMORIA;//pdf dice int
+char* IP_CPU;
+char* PUERTO_CPU_DISPATCH;//pdf dice int
+char* PUERTO_CPU_INTERRUPT;//pdf dice int
+char* ALGORITMO_PLANIFICACION;
+int QUANTUM;
+char** RECURSOS;//lista, luego usar common de string
+char** INSTANCIAS_RECURSOS;//lista, luego usar common de string
+int GRADO_MULTIPROGRAMACION;
+
+t_log* kernel_logger;
+t_log* kernel_debug;
 
 #endif
